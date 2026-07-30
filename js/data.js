c93ed347f26265a638da372853563e9d050441ee
MMOSHII <94529585+MMOSHII@users.noreply.github.com>
2026-07-30 13:32:56 +0700

    A little change

---
+++ js/data.js
@@ -0,0 +1,8 @@
+const pages = [
+    {
+        name: "Image to PDF Converter",
+        description: "Convert, crop, rotate images into customized PDF documents with margin controls.",
+        filename: "imgToPdf.html",
+        folder: "pages"
+    },
+];
